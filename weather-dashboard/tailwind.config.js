a1b144108ef2846725dc612ec894bc4cd7493da8
C7-mia <akidirita4@gmail.com>
2025-08-21 13:42:18 +0300

    feat: weather-dashboard initial implementation with React, Tailwind, Axios

---
+++ weather-dashboard/tailwind.config.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+=======
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+>>>>>>> 6bfd11f (feat: weather-dashboard initial implementation with React, Tailwind, Axios)
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+<<<<<<< HEAD
+};
+=======
+}
+
+>>>>>>> 6bfd11f (feat: weather-dashboard initial implementation with React, Tailwind, Axios)
