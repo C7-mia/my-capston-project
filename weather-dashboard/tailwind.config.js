50a48e45e081966b879d78df54aab144261ac3bf
C7-mia <akidirita4@gmail.com>
2025-09-01 05:12:27 +0300

    Update tailwind.config.js content paths

--- weather-dashboard/tailwind.config.js
+++ weather-dashboard/tailwind.config.js
@@ -1,21 +1,11 @@
-<<<<<<< HEAD
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}"
   ],
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
->>>>>>> 6bfd11f (feat: weather-dashboard initial implementation with React, Tailwind, Axios)
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-<<<<<<< HEAD
-};
-=======
+  plugins: []
 }
-
->>>>>>> 6bfd11f (feat: weather-dashboard initial implementation with React, Tailwind, Axios)
