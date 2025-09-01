c50c95afcb5bb0bc2759d279d8e048286030425c
C7-mia <akidirita4@gmail.com>
2025-09-01 04:38:58 +0300

    Update units.js

--- weather-dashboard/src/utils/units.js
+++ weather-dashboard/src/utils/units.js
@@ -1,2 +1,7 @@
-export const cToF = (c) => (c * 9) / 5 + 32;
-export const fToC = (f) => ((f - 32) * 5) / 9;
+export function cToF(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+export function fToC(fahrenheit) {
+  return ((fahrenheit - 32) * 5) / 9;
+}
